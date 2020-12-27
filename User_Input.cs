2e09285b399740bf350c7d80a4d655dec20b3543
ShawnGester <sge23@wisc.edu>
2020-12-26 21:33:16 -0600

    Created repo, updated scripts to create, display, and animate cards

---
+++ User_Input.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User_Input : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMouseClick();
+    }
+
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    Deck();
+                }
+                else if (hit.collider.CompareTag("Card"))
+                {
+                    Card();
+                }
+                else if (hit.collider.CompareTag("Top"))
+                {
+                    Top();
+                }
+                else if (hit.collider.CompareTag("Bottom"))
+                {
+                    Bottom();
+                }
+            }
+        }
+    }
+
+    void Deck() {
+        print("clicked on Deck");
+    }
+
+    void Card() {
+        print("clicked on Card");
+
+    }
+
+    void Top() {
+        print("clicked on Top");
+
+    }
+
+    void Bottom() {
+        print("clicked on Bottom");
+
+    }
+}
